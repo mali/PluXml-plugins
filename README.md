8fa77c72e47bfbb603d956c74d97e3c65e9921da
Laurent Navet <laurent.navet@gmail.com>
2014-01-06 21:30:27 +0100

    add slimbox2 plugin

--- README.md
+++ README.md
@@ -4,5 +4,10 @@ Here are my plugins for PluXml[1] blog.
   is a plugin for clustrmap[2], it loads the needed javascript on every page.
   just copy it in plugins/, it doesn't display the map, you need de do it yourself.
 
+* slimbox2
+  is a plugin for slimbox2[3]. Need the pluxml jquery plugin
+  an example here: http://blog.naturalevent.fr/article17/plugin-slimbox2
+
 [1] www.pluxml.org
 [2] www.clustrmaps.com
+[3] http://www.digitalia.be/software/slimbox2
---
+++ slimbox2/slimbox2.php
@@ -0,0 +1,39 @@
+﻿<?php
+/**
+ * Plugin slimbox2
+ *
+ * @package	PLX
+ * @version	2.0.4
+ * @date	06/09/2012
+ * @author	Laurent Navet (Mali)
+ **/
+
+class slimbox2 extends plxPlugin {
+	/**
+	 * Constructeur de la classe slimbox2
+	 *
+	 * @param	default_lang	langue par défaut utilisée par PluXml
+	 * @return	null
+	 * @author	Laurent Navet
+	 **/
+
+	public function __construct($default_lang) {
+
+		# Appel du constructeur de la classe plxPlugin (obligatoire)
+		parent::__construct($default_lang);
+		# Déclarations des hooks
+		$this->addHook('ThemeEndHead', 'addSlimbox2');
+		$this->addHook('AdminTopEndHead', 'addSlimbox2');
+	 }
+	/**
+	 * Méthode qui ajoute l'insertion du fichier javascript de slimbox2 dans la partie <head> du site
+	 *
+	 * @return	stdio
+	 * @author	Laurent Navet
+	 **/
+	public function addSlimbox2() {
+		echo "\t".'<link rel="stylesheet" href="'.PLX_PLUGINS.'slimbox2/css/slimbox2.css" type="text/css" media="screen" />'."\n";
+		echo "\t".'<script type="text/javascript" src="'.PLX_PLUGINS.'slimbox2/js/slimbox2.js"></script>'."\n";
+	}
+}
+?>
